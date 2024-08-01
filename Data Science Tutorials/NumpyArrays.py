edacdc3d3097d71e1781144fd1c4b88043e53248
Tarik Gökmen <tarikgokmen1999@gmail.com>
2024-08-01 15:15:51 +0200

    Data Science Basic Tutorials finished

--- Data Science Tutorials/NumpyArrays.py
+++ Data Science Tutorials/NumpyArrays.py
@@ -1,4 +1,5 @@
 import numpy as np
+#print(np.__version__)
 
 height = [1.87,  1.87, 1.82, 1.91, 1.90, 1.85]
 weight = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
@@ -7,3 +8,14 @@ np_height = np.array(height)
 np_weight = np.array(weight)
 
 print(type(np_height))
+print(type(np_weight))
+
+# Calculate bmi
+bmi = np_weight / np_height ** 2
+
+# Print the result
+print(bmi)
+
+#bmi > 23
+
+print(bmi[bmi < 25])
