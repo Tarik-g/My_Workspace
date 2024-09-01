2694221bc5dc2027fbcf390c194865cbdbfefbe8
Tarik Gökmen <tarikgokmen1999@gmail.com>
2024-09-01 15:34:39 +0200

    algos continued

--- algorithms/array_algorithms.py
+++ algorithms/array_algorithms.py
@@ -37,7 +37,7 @@ def maximum_subarray_product(arr):
     return max_so_far
 
 a = [1, -2, -3, 0, 7, -8, -2]
-print("Maximum contiguous product is", maximum_subarray_product(a))
+# print("Maximum contiguous product is", maximum_subarray_product(a))
 
 # O(n)
 def find_missing_number(arr):
@@ -50,3 +50,59 @@ def find_missing_number(arr):
 
 a = [1, 2, 3, 5, 7, 6, 8]
 # print(find_missing_number(a))
+
+# O(n)
+def equilibrium_index(arr):
+    if len(arr) < 3:
+        return -1
+    
+    left = 0
+    pivot = 0
+    right = sum(arr[1:])
+
+    while pivot < len(arr) - 1 and right != left:
+        pivot += 1
+        right -= arr[pivot]
+        left += arr[pivot - 1]
+    
+    if left == right:
+        return pivot
+    else:
+        return -1
+
+a = [1, 7, 3, 6, 5, 6]
+# print(equilibrium_index(a))
+
+# O(n)
+def array_leaders(arr):
+    ans = [arr[-1]]
+    n = len(arr)
+    max_right = arr[-1]
+    for i in range(n - 2, -1, -1):
+        if arr[i] > max_right:
+            ans.append(arr[i])
+            max_right = arr[i]
+    return ans
+
+a = [16, 17, 4, 3, 5, 2]
+# print(array_leaders(a))
+
+# O(n)
+def rotate_k_times(arr, k):
+    n = len(arr)
+    k = k % n
+    rotated_arr = arr[(n - k): n] + arr[:(n - k)]
+    return rotated_arr
+
+a = [16, 17, 4, 3, 5, 2]
+# print(rotate_k_times(a, 1))
+# print(rotate_k_times(a, 2))
+# print(rotate_k_times(a, 3))
+# print(rotate_k_times(a, 7))
+
+def kth_smallest(arr, k):
+    arr.sort()
+    return arr[k-1]
+
+a = [16, 17, 4, 3, 5, 2]
+# print(kth_smallest(a, 3))
---
+++ algorithms/string_algorithms.py
@@ -0,0 +1,5 @@
+def longest_palindromic_substring(st):
+    longest_so_far = ""
+    longest_current = ""
+    for char in st:
+        
