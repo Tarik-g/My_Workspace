39cb6f65fbd797be7b731e8a4fd916cfa437f130
Tarik Gökmen <tarikgokmen1999@gmail.com>
2024-09-09 16:02:03 +0200

    some langueage tests

---
+++ c++test/test.cpp
@@ -0,0 +1,5 @@
+#include <iostream>                        // Ein- und Ausgabebibliothek
+
+int main(){                                // Hauptfunktion
+    std::cout << "Hallo, du schöne Welt!"; // Ausgabe eines Textes
+}
