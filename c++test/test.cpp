bca5ff6a3df8a983839eb5e7fbf0c8616d7aa505
Tarik-g <tarikgokmen1999@gmail.com>
2025-02-27 11:17:21 +0100

    refactor: Change directory structure

--- c++test/test.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>                        // Ein- und Ausgabebibliothek
-
-int main(){                                // Hauptfunktion
-    std::cout << "Hallo, du schöne Welt!"; // Ausgabe eines Textes
-}
